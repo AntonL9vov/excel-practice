bbd4a2175900f385860fc9a104f9d5a2efcea486
AntonLvov <lvov.anton.02@gmail.com>
2021-10-07 16:02:31 +0300

    Finish basic framework functional

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,23 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {resizeHandler} from '@/components/table/table.resize';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    });
+  }
+
   toHLTM() {
     return createTable(100)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      resizeHandler(this.$root, event)
+    }
   }
 }
